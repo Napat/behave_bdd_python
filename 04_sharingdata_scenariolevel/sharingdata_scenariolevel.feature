efbe9afe38c347cfc007544c4b53240b344a6d5e
Napat Rungruangbangchan <napat.joe@gmail.com>
2016-07-28 19:03:53 +0700

    Sharing data between step(scenario level) using 'context' object

---
+++ 04_sharingdata_scenariolevel/sharingdata_scenariolevel.feature
@@ -0,0 +1,15 @@
+# Pass around data from step to step
+# Second scenario will fail because context is not sharing between scenario.
+
+Feature: Sharing data scenario level using context
+
+    Scenario: Process refund by order id
+
+        Given I find order id from database
+        When I issue a refund for that order id
+        Then process payment to user
+
+    Scenario: Refund should fail on a refunded item
+
+        When I issue a refund on the same order
+        Then refund process will fail
---
+++ 04_sharingdata_scenariolevel/steps/steps_sharingdata.py
@@ -0,0 +1,44 @@
+
+from behave import given, when, then
+
+#----------------------------------------------------------------------------------------------------------------------#
+@given('I find order id from database')
+def find_orderid_from_db(context):
+
+    print("Finding an order from the database....")
+    context.order_id = '12481632'
+
+    print("Found an orders. Order number: {}".format(context.order_id))
+
+#----------------------------------------------------------------------------------------------------------------------#
+@when('I issue a refund for that order id')
+def issue_refund(context):
+
+    print("Trying to issue a refund for order number: {}".format(context.order_id))
+    context.refund_stat = refund_by_id(context.order_id)
+
+@when('I issue a refund on the same order')
+def issue_repeat_refund(context):
+
+    print("Trying to issue refund on same order: {}".format(context.order_id))
+
+#----------------------------------------------------------------------------------------------------------------------#
+@then('process payment to user')
+def payment_should_process(context):
+	if context.refund_stat == true:
+	    print("Payment successfully processed")
+	    print("Payment is for refund of order number: {}".format(context.order_id))
+	else:
+		raise Exception("Unexpected Payment error.")
+
+@then('refund process will fail')
+def refund_fails(context):
+
+    print("The refund for order {} fail.".format(context.order_id))
+
+#--------------------------------------------
+# operation functions
+
+def refund_by_id(uid):
+	print("Refund")
+	return true
